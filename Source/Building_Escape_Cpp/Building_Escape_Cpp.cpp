c9d5a307aef06b15b97d78fe411e126e77e09237
Nuggsy <53103113+Golden-Nuggs@users.noreply.github.com>
2020-01-24 03:03:22 +0000

    First commit

---
+++ Source/Building_Escape_Cpp/Building_Escape_Cpp.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Building_Escape_Cpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Building_Escape_Cpp, "Building_Escape_Cpp" );
---
+++ Source/Building_Escape_Cpp/Building_Escape_CppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_Escape_CppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_CPP_API ABuilding_Escape_CppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
